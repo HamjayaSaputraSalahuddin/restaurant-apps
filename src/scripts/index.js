1b9561cb4fce0c465b3591ff48903aedff5b2eb6
Hamjaya Saputra Salahuddin <hamjayasaputra35@gmail.com>
2022-12-04 20:13:16 +0800

    initiation eslint

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,22 @@
-import '../scripts/data-source.js';
+import './data-source';
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/responsive.css';
 import '../styles/main.css';
-
 
 const menu = document.querySelector('#menu');
 const drawer = document.querySelector('#drawer');
 const hero = document.querySelector('.hero');
 const main = document.querySelector('main');
 
-menu.addEventListener('click', function (event) {
-    drawer.classList.toggle('open');
-    event.stopPropagation();
+menu.addEventListener('click', (event) => {
+  drawer.classList.toggle('open');
+  event.stopPropagation();
 });
 
-hero.addEventListener('click', function() {
-    drawer.classList.remove('open');
+hero.addEventListener('click', () => {
+  drawer.classList.remove('open');
 });
 
-main.addEventListener('click', function () {
-    drawer.classList.remove('open');
-})
+main.addEventListener('click', () => {
+  drawer.classList.remove('open');
+});
